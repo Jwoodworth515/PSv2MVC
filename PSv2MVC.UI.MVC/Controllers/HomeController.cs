a280b5647c58ae212ca8b24ca89bd2f62185331b
Jacob Woodworth <jwoodworth515@gmail.com>
2021-06-28 18:29:31 -0500

    CommitAddingAdditionalPages

--- PSv2MVC.UI.MVC/Controllers/HomeController.cs
+++ PSv2MVC.UI.MVC/Controllers/HomeController.cs
@@ -14,16 +14,120 @@ namespace StoreFrontV3.UI.MVC.Controllers
         //[Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult Resume()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Links()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Projects()
+        {
             return View();
         }
 
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult Music()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DefiancePointe()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult VictoryForVillains()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult FromSkiesOfFire()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GiveThemMyName()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails2()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails3()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails4()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails5()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails6()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails7()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails8()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails9()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ProjectDetails10()
+        {
             return View();
         }
     }
