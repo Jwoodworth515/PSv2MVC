adcd26daa882389ba4f371c142a6a89f869e2a6c
Jacob Woodworth <jwoodworth515@gmail.com>
2021-07-14 18:27:12 -0500

    CommitBundleConfig

--- PSv2MVC.UI.MVC/App_Start/BundleConfig.cs
+++ PSv2MVC.UI.MVC/App_Start/BundleConfig.cs
@@ -7,8 +7,8 @@ namespace StoreFrontV3.UI.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -18,13 +18,34 @@ namespace StoreFrontV3.UI.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/template").Include(
+                "~/Scripts/js/main.js",
+                "~/Content/vendor/aos/aos.js",
+                "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                "~/Content/vendor/glightbox/js/glightbox.min.js",
+                "~/Content/vendor/isotope-layout/isotope.pkgd.min.js",
+                "~/Content/vendor/purecounter/purecounter.js",
+                "~/Content/vendor/swiper/swiper-bundle.min.js",
+                "~/Content/vendor/waypoints/noframework.waypoints.js"
+                ));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/img/favicon.png",
+                      "~/Content/img/apple-touch-icon.png",
+                      "~/Content/vendor/aos/aos.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/bootstrap-icons/bootstrap-icons.css",
+                      "~/Content/vendor/boxicons/css/boxicons.min.css",
+                      "~/Content/vendor/glightbox/css/glightbox.min.css",
+                      "~/Content/vendor/swiper/swiper-bundle.min.css",
+                      "~/Content/css/style.css"
+                      ));
         }
     }
 }
